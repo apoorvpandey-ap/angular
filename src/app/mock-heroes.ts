7fb1d3b495c2c803486c1f0db40d767f78a36a77
apoorv <apoorvcreate@gmail.com>
2020-07-25 15:02:05 +0530

    add service

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,12 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+
+  { id: 14, name: 'Sankal Pandey' },
+  { id: 15, name: 'Prabhat Pandey' },
+  { id: 16, name: 'Mohan Chaubey' },
+  { id: 17, name: 'Rishabh singh' },
+  { id: 18, name: 'Ankita Ehauhan' },
+  { id: 19, name: 'Shyamali Singh' },
+  { id: 20, name: 'Hema Malini' }
+];
